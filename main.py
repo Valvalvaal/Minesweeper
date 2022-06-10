2469463601f4901695f5887d78a20d9cb3a57675
Valeria Marin <valeriamarin193@gmail.com>
2022-06-10 14:44:00 -0500

    Added grey window

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ from tkinter import *
 
 root = Tk()
 
+root.configure(bg="#c6c6c6")
+# Change the settings of the window
+root.geometry("1440x720")
+root.title("Minesweeper")
+root.resizable(False, False)
+
+top_frame = Frame(root, bg="black", width=1440, height=180)  # Change later
+
+# Run the window
 root.mainloop()  # Open until x is clicked
