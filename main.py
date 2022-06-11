e1bf3ddab42cc0c8c61082b7eb4aeb1b1aa7b6f7
Valeria Marin <valeriamarin193@gmail.com>
2022-06-10 19:42:21 -0500

    Added top and left frames

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ root.geometry("1440x720")
 root.title("Minesweeper")
 root.resizable(False, False)
 
-top_frame = Frame(root, bg="black", width=1440, height=180)  # Change later
+top_frame = Frame(root,
+                  bg="black",  # Change later
+                  width=1440,
+                  height=180)
+
+top_frame.place(x=0, y=0)  # Set starting point according to pixels
+
+left_frame = Frame(root,
+                   bg='blue',  # Change later
+                   width=360,
+                   height=540
+                   )
+
+left_frame.place(x=0, y=0)
 
 # Run the window
 root.mainloop()  # Open until x is clicked
