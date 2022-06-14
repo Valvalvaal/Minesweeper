44b93609f4d47dec3a41dfed79965c4a260890a2
Valeria Marin <valeriamarin193@gmail.com>
2022-06-14 11:57:06 -0500

    Added center frame and changed hard coded size values

--- main.py
+++ main.py
@@ -4,26 +4,34 @@ import utils
 
 root = Tk()
 
-root.configure(bg="#c6c6c6")
+root.configure(bg=settings.COLOR)
 # Change the settings of the window
 root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("Minesweeper")
 root.resizable(False, False)
 
 top_frame = Frame(root,
-                  bg="black",  # Change later
-                  width=1440,
+                  bg=settings.COLOR,  # Change later
+                  width=settings.WIDTH,
                   height=utils.height_pct(25))
 
 top_frame.place(x=0, y=0)  # Set starting point according to pixels
 
 left_frame = Frame(root,
-                   bg='blue',  # Change later
-                   width=360,
-                   height=540
+                   bg=settings.COLOR,  # Change later
+                   width=utils.width_pct(25),
+                   height=utils.height_pct(75)
                    )
 
-left_frame.place(x=0, y=180)
+left_frame.place(x=0, y=utils.height_pct(25))
+
+center_frame = Frame(root,
+                     bg=settings.COLOR,
+                     width=utils.width_pct(75),
+                     height=utils.height_pct(75)
+                     )
+
+center_frame.place(x=utils.width_pct(25), y=utils.height_pct(25))
 
 # Run the window
 root.mainloop()  # Open until x is clicked
