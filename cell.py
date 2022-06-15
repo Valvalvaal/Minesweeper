ef0ad49c7fd16cdb5fc2fc48ab93c40935c8b5af
Valeria Marin <valeriamarin193@gmail.com>
2022-06-15 10:01:56 -0500

    Added actions on click

--- cell.py
+++ cell.py
@@ -9,13 +9,19 @@ class Cell:
     def create_btn_object(self, location):
         btn = Button(
             location,
+            width=12,
+            height=4,
             text='text'
         )
 
         # Assign an event to a button with bind method
-        btn.bind('<Button-1>', self.left_click_actions)
+        btn.bind('<Button-1>', self.left_click_actions)  # Left click
+        btn.bind('<Button-2>', self.right_click_actions)  # Right click
 
         self.cell_btn_object = btn
 
-    def left_click_actions(self):
+    def left_click_actions(self, event):
         print('I am left clicked!')
+
+    def right_click_actions(self, event):
+        print('I am right clicked!')
