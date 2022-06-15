c06649a7e9269d849b415881f9f3d99fdbffb65b
Valeria Marin <valeriamarin193@gmail.com>
2022-06-15 07:48:05 -0500

    Created cell class and button object

--- cell.py
+++ cell.py
@@ -1,3 +1,21 @@
+from tkinter import Button
+
+
 class Cell:
     def __init__(self, is_mine=False):
-        cell.is_mine = is_mine
+        self.is_mine = is_mine
+        self.cell_btn_object = None
+
+    def create_btn_object(self, location):
+        btn = Button(
+            location,
+            text='text'
+        )
+
+        # Assign an event to a button with bind method
+        btn.bind('<Button-1>', self.left_click_actions)
+
+        self.cell_btn_object = btn
+
+    def left_click_actions(self):
+        print('I am left clicked!')
